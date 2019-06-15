abaa45f456c44a94ebc8f9e951eea046d6c48171
zvezdin <git@zvezd.in>
2019-06-16 00:30:17 +0300

    Add fake event stream

--- nietzsche-backend/src/main/kotlin/app.kt
+++ nietzsche-backend/src/main/kotlin/app.kt
@@ -1,8 +1,11 @@
+import Data.ActivityEvent
+import Data.ActivityScoringMultiplier
 import Data.User
 import org.apache.ignite.Ignition
 import org.apache.ignite.cache.query.ScanQuery
 import java.nio.file.Paths
-
+import org.apache.ignite.IgniteDataStreamer
+import java.util.*
 
 
 fun main() {
@@ -22,4 +25,12 @@ fun main() {
     println(users.get(0))
 
     println("Hello world")
+
+
+
+    ignite.dataStreamer<Int, ActivityEvent>("activity_event_stream").use { stmr ->
+        // Stream entries.
+        for (i in 0..30)
+            stmr.addData(i, ActivityEvent(0, ActivityScoringMultiplier.RUNNING, Date(2017,1,i), i) )
+    }
 }
