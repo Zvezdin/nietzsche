49916a8ec912437825f36868a03ad23c19e512cc
zvezdin <git@zvezd.in>
2019-06-15 20:57:36 +0300

    Initial ignite project

---
+++ nietzsche-backend/src/main/java/main.java
@@ -0,0 +1,11 @@
+import org.apache.ignite.*;
+
+public class main {
+
+    public static void main(String args[]) {
+        Ignite i = Ignition.start();
+
+        System.out.println("zdr");
+
+    }
+}
