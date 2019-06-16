9b87e7960e4b259eb3e55606133c3babbf335576
Viktor Velev <viktorvelev8@gmail.com>
2019-06-16 12:43:22 +0300

    Basic merge

--- nietzsche/src/Main.js
+++ nietzsche/src/Main.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Dimensions } from 'react-native'
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 
 import { Button } from 'react-native-elements';
 import { styles } from './styles/styles'
+import { TabView, SceneMap } from 'react-native-tab-view';
 
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import ActiveView from './views/Active'
 import RecommendedView from './views/Recommended'
 import MarketplaceView from './views/Marketplace'
 
-const MainNavigator = createStackNavigator({
-    Active: { screen: ActiveView },
-    Recommended: { screen: RecommendedView },
-    Marketplace: { screen: MarketplaceView },
-    
-});
+class Main extends React.Component {
+    state = {
+        index: 0,
+        routes: [
+            { key: 'first', title: 'Active' },
+            { key: 'second', title: 'Marketplace' },
+            { key: 'third', title: 'Recommended' },
+        ],
+    };
 
-const Main = () => <ActiveView/>;
+    render() {
+        return (
+            <TabView
+                navigationState={this.state}
+                renderScene={SceneMap({
+                    first: ActiveView,
+                    second: MarketplaceView,
+                    third: RecommendedView
+                })}
+                onIndexChange={index => this.setState({ index })}
+                initialLayout={{ width: Dimensions.get('window').width }}
+            />
+        );
+    }
+}
 
 export default Main;
